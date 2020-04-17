d5c23a034be8a38d13a1234cbc6c3c76454ce2b8
rafaelferrari0 <43826960+rafaelferrari0@users.noreply.github.com>
2020-04-17 16:51:40 -0300

    created to compile with mingw64

---
+++ build2/mingw64/2nd-make.bat
@@ -0,0 +1,24 @@
+@ECHO OFF
+
+
+PUSHD %~dp0
+
+FOR /D %%i IN ("..\","..\","..\","..\","..\","..\") DO (
+  CD %%i
+  IF EXIST bin\mingw32-make.exe SET MINGW_HOME=%%~fi
+)
+
+IF "%MINGW_HOME%"=="" (
+ECHO MinGW directory not found
+  GOTO fim
+)
+
+ECHO MINGW_HOME=%MINGW_HOME%
+
+POPD
+
+SET PATH=%MINGW_HOME%\x86_64-w64-mingw32\bin;%MINGW_HOME%\bin;%MINGW_HOME%\cmake\bin;%PATH%
+
+mingw32-make.exe
+
+pause
